4b5337c3e016e27f42ad5e63b9ecc599d583452c
Yakov Kolyada <yakov.kolyada@gmail.com>
2022-07-21 09:12:43 +0300

    020201 PublisherGui v1.1

--- 02AdvancedClassDesign/0202StaticAndFinalKeywords/PublisherGui.java
+++ 02AdvancedClassDesign/0202StaticAndFinalKeywords/PublisherGui.java
@@ -6,11 +6,12 @@ class Book {
 	}
 }
 
-public class Publisher {
-	public static void main(String args[]) {
+public class PublisherGui {
+	public static void main() {
+		//System.out.println(Book);	
 		System.out.println(Book.bookCount);
 		Book b1 = new Book();
 		Book b2 = new Book();
-		System.out.println(Book.bookCount);
+		System.out.println(Book.bookCount);		
 	}
 }
