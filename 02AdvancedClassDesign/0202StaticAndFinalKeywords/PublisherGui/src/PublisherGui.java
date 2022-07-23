eb2e66dc0281bb1d777a052bf46ba0fab11b6d9c
Yakov Kolyada <yakov.kolyada@gmail.com>
2022-07-23 18:58:34 +0300

    020201 PublisherGui v1.2

--- 02AdvancedClassDesign/0202StaticAndFinalKeywords/PublisherGui/src/PublisherGui.java
+++ 02AdvancedClassDesign/0202StaticAndFinalKeywords/PublisherGui/src/PublisherGui.java
@@ -1,16 +1,53 @@
 class Book {
-	static int bookCount;
+	// public static int bookCount = initResult();
+	public static int bookCount = 5;
+
+	static {
+		// bookCount = 5;
+		if (bookCount < 10) { 
+			bookCount = initResult();
+		}
+		
+	}
 
 	public Book() {
 		++bookCount;
+	}
+
+	public static void printBookCount() {
+		System.out.println(bookCount);
+	}
+
+	public static int getBookCount() {
+		return bookCount;
+	}
+
+	public static void incrementBookCount() {
+		++bookCount;
+	}
+
+	// public int initResult() {
+	static int initResult() {
+		return 20;
 	}
 }
 
 public class PublisherGui {
 	public static void main(String[] args) {
+		System.out.println(Book.class);
+		System.out.println("bookCount: " + Book.bookCount);
+		Book.incrementBookCount();
 		System.out.println(Book.bookCount);
 		Book b1 = new Book();
 		Book b2 = new Book();
-		System.out.println(Book.bookCount);		
+		System.out.println(Book.bookCount);
+		b1.bookCount = 5;
+		System.out.println(Book.bookCount);
+		System.out.println(b2.bookCount);
+		Book.printBookCount();
+		
+		StaticException staticExp = new StaticException();
+		System.out.println(StaticException.class);
+		System.out.println("userAccount: " + staticExp.userAccount);
 	}
 }
